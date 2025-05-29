cd7af0236e530eed47a90e3f290d53a42a30306b
shaker <hydrashaker@gmail.com>
2025-05-29 17:50:26 +0530

    changes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,7 @@
 import { Slot } from "expo-router";
 import SafeScreen from "@/components/SafeScreen"
 import { ClerkProvider, tokenCache } from "@clerk/clerk-expo";
-import { StatusBar } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
